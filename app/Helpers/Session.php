c383d30fb6fbc3a325f526cf9be0361609756aff
lahcen404 <lahcen.maskour2003@gmail.com>
2026-01-20 09:15:06 +0100

    create start user session

---
+++ app/Helpers/Session.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\Helpers;
+
+use app\Models\User;
+
+class Session
+{
+
+    private function startUserSession(User $user): void
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        
+        $_SESSION['user_id'] = $user->getId();
+        $_SESSION['user_name'] = $user->getFullName();
+        
+        
+        $_SESSION['user_role'] = $user->getRole()->value; 
+    }
+} 
+
