20a24925131fb89eac05ef67170ad57995a6b8c4
lahcen404 <lahcen.maskour2003@gmail.com>
2026-01-20 14:55:10 +0100

    create CRUD of users

--- app/DAOs/UserDAO.php
+++ app/DAOs/UserDAO.php
@@ -4,6 +4,7 @@ namespace app\DAOs;
 
 use app\Models\User;
 use config\DBConnection;
+use PDO;
 
 class UserDAO{
 
@@ -24,4 +25,52 @@ class UserDAO{
         return $data ?: null;
 
     }
+
+    public function create(array $data): bool{
+
+        $sql = 'INSERT INTO users (fullName,email,password,role) VALUES (:fullName, :email, :password, :role)';
+        $stmt = $this->db->prepare($sql);
+
+        return $stmt->execute([
+            'fullName' => $data['fullName'],
+            'email' => $data['email'],
+            'password' => $data['password'],
+            'role' => $data['role']
+        ]);
+    }
+
+      public function update(int $id, array $data): bool {
+        $sql = "UPDATE users SET fullname = :fullname, email = :email, role = :role 
+                WHERE id = :id";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute([
+            'id'       => $id,
+            'fullname' => $data['fullName'],
+            'email'    => $data['email'],
+            'role'     => $data['role']
+        ]);
+    }
+
+    public function delete(int $id): bool {
+        $sql = "DELETE FROM users WHERE id = :id";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute(['id' => $id]);
+    }
+
+    
+    public function getAllUsers(): array {
+        $sql = "SELECT * FROM users ORDER BY id DESC";
+        return $this->db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getUserById(int $id): ?array {
+        $sql = "SELECT * FROM users WHERE id = :id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute(['id' => $id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC) ?: null;
+    }
+
+
+
+
 }
