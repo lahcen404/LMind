9d11337b1418edb2328cfefb63feb54873a93f00
lahcen404 <lahcen.maskour2003@gmail.com>
2026-01-22 11:43:47 +0100

    create SprintService

---
+++ app/Repositories/SprintRepository.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace app\Repositories;
+
+use app\DAOs\SprintDAO;
+use app\Mappers\SprintMapper;
+use app\Models\Sprint;
+
+class SprintRepository
+{
+    private static ?SprintRepository $instance = null;
+
+    // singleton instance
+    public static function getInstance(): SprintRepository
+    {
+        if (self::$instance === null) {
+            self::$instance = new SprintRepository();
+        }
+        return self::$instance;
+    }
+
+    private function __construct() {}
+
+    // find all
+    public function findAll(): array
+    {
+        $sprintDAO = SprintDAO::getInstance();
+        $rawData = $sprintDAO->getAll();
+        
+        foreach($rawData as $row){
+            $classes[] = SprintMapper::toEntity($row);
+        }
+
+        return $classes;
+    }
+
+    // find Sprint by id
+    public function findById(int $id): ?Sprint
+    {
+        $sprintDAO = SprintDAO::getInstance();
+        $data = $sprintDAO->findById($id);
+        
+        return $data ? SprintMapper::toEntity($data) : null;
+    }
+
+    // create Sprint
+    public function save(Sprint $sprint): bool
+    {
+        $sprintDAO = SprintDAO::getInstance();
+        $data = SprintMapper::toArray($sprint);
+        
+        return $sprintDAO->create($data);
+    }
+
+    // update Spiint
+    public function update(int $id, Sprint $sprint): bool
+    {
+        $sprintDAO = SprintDAO::getInstance();
+        $data = SprintMapper::toArray($sprint);
+        
+        return $sprintDAO->update($id, $data);
+    }
+
+    // delete Spriint
+    public function delete(int $id): bool
+    {
+        $sprintDAO = SprintDAO::getInstance();
+        return $sprintDAO->delete($id);
+    }
+}
